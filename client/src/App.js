2d9ef874ef98f491ffed36c6f5ab56b0cdb4ff36
sandeep <sandeep.vishwakarma.sfdc@gmail.com>
2021-02-28 12:24:28 +0530

    insert and read operation

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
 function App() {
+  let [name,setName] = useState("");
+  let [age,setAge] = useState(0);
+  let [lst_friend,setListFriend] = useState([]);
+  let addFriend=()=>{
+    Axios.post('http://localhost:5000/insert',{name:name,age:age}).then(response=>{
+      console.log('Response ',response);
+      setListFriend(response.data);
+    }).catch(err=> console.log('ERR'+err));
+  }
+
+  useEffect(()=>{
+    Axios.get('http://localhost:5000/read').then(res=>{
+      console.log('response ',res.data);
+      setListFriend(res.data);
+    })
+  },[]);
+
   return (
     <div className="App">
-      Hello world thik hai ok
+     <div className="input">
+      <input name="name" type="text" placeholder="Enter name" onChange={(event)=>setName(event.target.value)}></input>
+      <input name="age" type="number" placeholder="Enter Age" onChange={(event)=>setAge(event.target.value)}></input>
+      <button onClick={addFriend}>Add Friend</button>
+    </div>
+
+    {lst_friend.map(ele=>{
+      return <div key={ele._id}>{ele.name} {ele.age}</div>
+    })}
+
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -2,17 +2,38 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const FriendModel = require('./models/Friends');
+const cors = require('cors');
 const DBSERVERURL = 'mongodb+srv://Sandeep:Sandeep@123@mernstack1db.fvnwp.mongodb.net/test'
 const DBLOCALURL = 'mongodb://localhost:27017/marnstackDB?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
 mongoose.connect(DBLOCALURL,{useNewUrlParser:true});
 
-app.get('/insert',async (req,res)=>{
+app.use(cors());
+app.use(express.json());
+
+app.post('/insert',async (req,res)=>{
    let friend = new FriendModel({
-       name:'sandeep',
-       age:22
+       name:req.body.name,
+       age:req.body.age
    });
    await friend.save();
-   res.send('Friend inserted');
+   FriendModel.find({},(err,result)=>{
+       if(result){
+           console.log('Result ',result);
+           res.send(result);
+       }
+   })
 });
 
+app.get('/read',async (req,res)=>{
+    FriendModel.find({},(err,result)=>{
+        if(err)
+            console.log('err->'+err);
+        else{
+            res.send(result);
+        }    
+    });
+});
+app.get('/',async (req,res)=>{
+    res.send('<h1>Home page</h1>');
+});
 app.listen(5000,()=>console.log('server is running'));
